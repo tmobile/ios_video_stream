bfed0d581b691dec32f46b8cde3ff3149c638c69
Pete Myron <pete.myron@t-mobile.com>
2021-03-29 15:49:09 -0700

    gofmt go files

--- main.go
+++ main.go
@@ -1,89 +1,89 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "os/signal"
-    "time"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
-    "go.nanomsg.org/mangos/v3"
-	  "go.nanomsg.org/mangos/v3/protocol/pull"
-	  // register transports
-	  _ "go.nanomsg.org/mangos/v3/transport/all"
-    
-	  log "github.com/sirupsen/logrus"
+	"go.nanomsg.org/mangos/v3"
+	"go.nanomsg.org/mangos/v3/protocol/pull"
+	// register transports
+	_ "go.nanomsg.org/mangos/v3/transport/all"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-    var udid       = flag.String( "udid"     , ""                    , "Device UDID" )
-    var streamCmd  = flag.Bool(   "stream"   , false                 , "Stream jpegs" )
-    var pullSpec   = flag.String( "pullSpec" , "tcp://127.0.0.1:7879", "NanoMsg spec to pull jpeg frames from" )
-    var iface      = flag.String( "interface", "none"                , "Network interface to listen on" )
-    var port       = flag.String( "port"     , "8000"                , "Network port to listen on" )
-    var verbose    = flag.Bool(   "v"        , false                 , "Verbose Debugging" )
-    var secure     = flag.Bool( "secure"     , false                 , "Secure HTTPS mode" )
-    var cert       = flag.String( "cert"     , "https-server.crt"    , "Cert for HTTP" )
-    var key        = flag.String( "key"      , "https-server.key"    , "Key for HTTPS" )
-    var coordinator= flag.String( "coordinator", "127.0.0.1:8027"    , "Coordinator control address" )
-    
-    flag.Parse()
-    
-    log.SetFormatter(&log.JSONFormatter{})
+	var udid = flag.String("udid", "", "Device UDID")
+	var streamCmd = flag.Bool("stream", false, "Stream jpegs")
+	var pullSpec = flag.String("pullSpec", "tcp://127.0.0.1:7879", "NanoMsg spec to pull jpeg frames from")
+	var iface = flag.String("interface", "none", "Network interface to listen on")
+	var port = flag.String("port", "8000", "Network port to listen on")
+	var verbose = flag.Bool("v", false, "Verbose Debugging")
+	var secure = flag.Bool("secure", false, "Secure HTTPS mode")
+	var cert = flag.String("cert", "https-server.crt", "Cert for HTTP")
+	var key = flag.String("key", "https-server.key", "Key for HTTPS")
+	var coordinator = flag.String("coordinator", "127.0.0.1:8027", "Coordinator control address")
 
-    if *verbose {
-        log.Info("Set Debug mode")
-        log.SetLevel(log.DebugLevel)
-    }
+	flag.Parse()
 
-    if *streamCmd {
-        stream( *pullSpec, *udid, *iface, *port, *secure, *cert, *key, *coordinator )
-    } else {
-        flag.Usage()
-    }
+	log.SetFormatter(&log.JSONFormatter{})
+
+	if *verbose {
+		log.Info("Set Debug mode")
+		log.SetLevel(log.DebugLevel)
+	}
+
+	if *streamCmd {
+		stream(*pullSpec, *udid, *iface, *port, *secure, *cert, *key, *coordinator)
+	} else {
+		flag.Usage()
+	}
 }
 
-func stream( pullSpec string, udid string, tunName string, port string, secure bool, cert string, key string, coordinator string ) {
-    pullSock := setup_nanomsg_sockets( pullSpec )    
-    
-    stopChannel := make( chan bool )
-    stopChannel2 := make( chan bool )
-    waitForSigInt( stopChannel, stopChannel2 )
-    
-    startJpegServer( pullSock, stopChannel2, port, tunName, secure, cert, key, coordinator, udid )
-        
-    <- stopChannel
+func stream(pullSpec string, udid string, tunName string, port string, secure bool, cert string, key string, coordinator string) {
+	pullSock := setup_nanomsg_sockets(pullSpec)
+
+	stopChannel := make(chan bool)
+	stopChannel2 := make(chan bool)
+	waitForSigInt(stopChannel, stopChannel2)
+
+	startJpegServer(pullSock, stopChannel2, port, tunName, secure, cert, key, coordinator, udid)
+
+	<-stopChannel
 }
 
-func setup_nanomsg_sockets( pullSpec string ) ( pullSock mangos.Socket ) {
-    var err error
-        
-    if pullSock, err = pull.NewSocket(); err != nil {
-        log.WithFields( log.Fields{
-            "type": "err_socket_new",
-            "zmq_spec": pullSpec,
-            "err": err,
-        } ).Fatal("Socket new error")
-    }
-    pullSock.SetOption( mangos.OptionRecvDeadline, time.Duration.Seconds( 1 ) )
-    if err = pullSock.Listen(pullSpec); err != nil {
-        log.WithFields( log.Fields{
-            "type": "err_socket_bind",
-            "spec": pullSpec,
-            "err": err,
-        } ).Fatal("Socket bind error")
-    }
-    return pullSock
+func setup_nanomsg_sockets(pullSpec string) (pullSock mangos.Socket) {
+	var err error
+
+	if pullSock, err = pull.NewSocket(); err != nil {
+		log.WithFields(log.Fields{
+			"type":     "err_socket_new",
+			"zmq_spec": pullSpec,
+			"err":      err,
+		}).Fatal("Socket new error")
+	}
+	pullSock.SetOption(mangos.OptionRecvDeadline, time.Duration.Seconds(1))
+	if err = pullSock.Listen(pullSpec); err != nil {
+		log.WithFields(log.Fields{
+			"type": "err_socket_bind",
+			"spec": pullSpec,
+			"err":  err,
+		}).Fatal("Socket bind error")
+	}
+	return pullSock
 }
 
-func waitForSigInt( stopChannel chan bool, stopChannel2 chan bool ) {
-    c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt)
-    go func() {
-        for sig := range c {
-            fmt.Printf("Got signal %s\n", sig)
-            go func() { stopChannel2 <- true }()
-            stopChannel <- true
-        }
-    }()
+func waitForSigInt(stopChannel chan bool, stopChannel2 chan bool) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			fmt.Printf("Got signal %s\n", sig)
+			go func() { stopChannel2 <- true }()
+			stopChannel <- true
+		}
+	}()
 }
--- server.go
+++ server.go
@@ -1,356 +1,357 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "html/template"
-    //"image"
-    _ "image/jpeg"
-    //"io"
-    "log"
-    "net"
-    "net/http"
-    "os"
-    "strings"
-    "sync"
-    //"time"
-    
-    "github.com/gorilla/websocket"
-    
-    "go.nanomsg.org/mangos/v3"
-	  // register transports
-	  _ "go.nanomsg.org/mangos/v3/transport/all"
-	  uj "github.com/nanoscopic/ujsonin/mod"
+	"bytes"
+	"fmt"
+	"html/template"
+	//"image"
+	_ "image/jpeg"
+	//"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	//"time"
+
+	"github.com/gorilla/websocket"
+
+	"go.nanomsg.org/mangos/v3"
+	// register transports
+	uj "github.com/nanoscopic/ujsonin/mod"
+	_ "go.nanomsg.org/mangos/v3/transport/all"
 )
 
-func callback( r *http.Request ) bool {
-    return true
+func callback(r *http.Request) bool {
+	return true
 }
 
-var upgrader = websocket.Upgrader {
-    CheckOrigin: callback,
+var upgrader = websocket.Upgrader{
+	CheckOrigin: callback,
 }
 
 type ImgMsg struct {
-    imgNum int
-    msg string
-    data []byte
+	imgNum int
+	msg    string
+	data   []byte
 }
 
 type ImgType struct {
-    rawData []byte
+	rawData []byte
 }
 
 const (
-    WriterStop = iota
+	WriterStop = iota
 )
 
 type WriterMsg struct {
-    msg int
+	msg int
 }
 
 type MainMsg struct {
-    msg int
+	msg int
 }
 
 const (
-    BeginDiscard = iota
-    EndDiscard
+	BeginDiscard = iota
+	EndDiscard
 )
 
 type Stats struct {
-    recv int
-    dumped int
-    sent int
-    socketConnected bool
-    waitCnt int
+	recv            int
+	dumped          int
+	sent            int
+	socketConnected bool
+	waitCnt         int
 }
 
-func startJpegServer( inSock mangos.Socket, stopChannel chan bool, mirrorPort string, tunName string, secure bool, cert string, key string, coordinator string, udid string ) {
-    var err error
-    
-    ifaces, err := net.Interfaces()
-    if err != nil {
-        fmt.Printf( err.Error() )
-        os.Exit( 1 )
-    }
-    
-    var listen_addr string
-    if tunName == "none" {
-        fmt.Printf("No tunnel specified; listening on all interfaces\n")
-        listen_addr = ":" + mirrorPort
-    } else {
-        foundInterface := false
-        for _, iface := range ifaces {
-            addrs, err := iface.Addrs()
-            if err != nil {
-                fmt.Printf( err.Error() )
-                os.Exit( 1 )
-            }
-            for _, addr := range addrs {
-                var ip net.IP
-                switch v := addr.(type) {
-                    case *net.IPNet:
-                        ip = v.IP
-                    case *net.IPAddr:
-                        ip = v.IP
-                    default:
-                        fmt.Printf("Unknown type\n")
-                }
-                if iface.Name == tunName {
-                    str := ip.String()
-					if !strings.Contains(str,":") {
+func startJpegServer(inSock mangos.Socket, stopChannel chan bool, mirrorPort string, tunName string, secure bool, cert string, key string, coordinator string, udid string) {
+	var err error
+
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Printf(err.Error())
+		os.Exit(1)
+	}
+
+	var listen_addr string
+	if tunName == "none" {
+		fmt.Printf("No tunnel specified; listening on all interfaces\n")
+		listen_addr = ":" + mirrorPort
+	} else {
+		foundInterface := false
+		for _, iface := range ifaces {
+			addrs, err := iface.Addrs()
+			if err != nil {
+				fmt.Printf(err.Error())
+				os.Exit(1)
+			}
+			for _, addr := range addrs {
+				var ip net.IP
+				switch v := addr.(type) {
+				case *net.IPNet:
+					ip = v.IP
+				case *net.IPAddr:
+					ip = v.IP
+				default:
+					fmt.Printf("Unknown type\n")
+				}
+				if iface.Name == tunName {
+					str := ip.String()
+					if !strings.Contains(str, ":") {
 						listen_addr = str + ":" + mirrorPort
 						foundInterface = true
 					}
-                }
-            }
-        }
-        if foundInterface == false {
-            fmt.Printf( "Could not find interface %s\n", tunName )
-            os.Exit( 1 )
-        }
-    }
-  
-    imgCh := make(chan ImgMsg, 60)
-    mainCh := make( chan MainMsg, 4 )
-    
-    lock := sync.RWMutex{}
-    var stats Stats = Stats{}
-    stats.recv = 0
-    stats.dumped = 0
-    stats.socketConnected = false
-    
-    statLock := sync.RWMutex{}
-    
-    sentSize := false
-    discard := true
-    
-    go func() {
-        imgnum := 1
-        
-        LOOP:
-        for {
-            select {
-                case <- stopChannel:
-                    fmt.Printf("Server channel got stop message\n")
-                    break LOOP
-                case msg := <- mainCh:
-                    if msg.msg == BeginDiscard {
-                        discard = true
-                    } else if msg.msg == EndDiscard {
-                        discard = false
-                    } 
-                default: // this makes the above read from stopChannel non-blocking
-            }
-            
-            msg, err := inSock.RecvMsg()
+				}
+			}
+		}
+		if foundInterface == false {
+			fmt.Printf("Could not find interface %s\n", tunName)
+			os.Exit(1)
+		}
+	}
 
-            fmt.Printf("Got incoming frame\n")
-            
-            if err != nil && err ==  mangos.ErrRecvTimeout {
-                continue
-            }
-            
-            if discard && sentSize {
-                statLock.Lock()
-                stats.dumped++
-                statLock.Unlock()
-                      
-                msg.Free()
-                continue
-            }
-            
-            clickScale := 1000
-            
-            imgMsg := ImgMsg{}
-            
-            // image is prepended by some JSON metadata
-            if msg.Body[0] == '{' {
-                endi := strings.Index( string(msg.Body), "}" )
-                //fmt.Printf( "out:[" + string( msg.Body[:endi+1] ) + "]" )
-                root, left := uj.Parse( msg.Body )
-                if ( len(msg.Body ) - len( left ) - 1 ) != endi {
-                    fmt.Printf( "size mistmatched what was parsed: %d != %d\n", endi, len( msg.Body ) - len(left) - 1 )
-                }
-                imgMsg.data = left
-                
-                ow := root.Get("ow").Int()
-                //oh := root.Get("og").Int()
-                dw := root.Get("dw").Int()
-                dh := root.Get("dh").Int()
-                if ow != dw {
-                    clickScale = ow / dw * 1000
-                }
-                
-                imgMsg.msg = fmt.Sprintf("Width: %d, Height: %d, Clickscale: %d, Size: %d\n", dw, dh, clickScale, len( msg.Body ) )
-                
-                if !sentSize {
-                    dataReader := bytes.NewBuffer( []byte( fmt.Sprintf( `{"type":"frame1","width":%d,"height":%d,"clickScale":%d,"uuid":"%s"}`, dw, dh, clickScale, udid ) ) )
-                    http.Post("http://"+coordinator+"/frame", "application/json", dataReader  )
-                    sentSize = true
-                }
-            } else {
-                imgMsg.data = msg.Body
-            }
-            
-            if !discard {
-                imgMsg.imgNum = imgnum
-                imgCh <- imgMsg
-                msg.Free()
-            } else {
-                msg.Free()
-            }
-            
-            statLock.Lock()
-            stats.recv++
-            statLock.Unlock()
+	imgCh := make(chan ImgMsg, 60)
+	mainCh := make(chan MainMsg, 4)
 
-            imgnum++
-        }
-    }()
-    
-    startServer( imgCh, mainCh, &lock, &statLock, &stats, listen_addr, secure, cert, key )
+	lock := sync.RWMutex{}
+	var stats Stats = Stats{}
+	stats.recv = 0
+	stats.dumped = 0
+	stats.socketConnected = false
+
+	statLock := sync.RWMutex{}
+
+	sentSize := false
+	discard := true
+
+	go func() {
+		imgnum := 1
+
+	LOOP:
+		for {
+			select {
+			case <-stopChannel:
+				fmt.Printf("Server channel got stop message\n")
+				break LOOP
+			case msg := <-mainCh:
+				if msg.msg == BeginDiscard {
+					discard = true
+				} else if msg.msg == EndDiscard {
+					discard = false
+				}
+			default: // this makes the above read from stopChannel non-blocking
+			}
+
+			msg, err := inSock.RecvMsg()
+
+			fmt.Printf("Got incoming frame\n")
+
+			if err != nil && err == mangos.ErrRecvTimeout {
+				continue
+			}
+
+			if discard && sentSize {
+				statLock.Lock()
+				stats.dumped++
+				statLock.Unlock()
+
+				msg.Free()
+				continue
+			}
+
+			clickScale := 1000
+
+			imgMsg := ImgMsg{}
+
+			// image is prepended by some JSON metadata
+			if msg.Body[0] == '{' {
+				endi := strings.Index(string(msg.Body), "}")
+				//fmt.Printf( "out:[" + string( msg.Body[:endi+1] ) + "]" )
+				root, left := uj.Parse(msg.Body)
+				if (len(msg.Body) - len(left) - 1) != endi {
+					fmt.Printf("size mistmatched what was parsed: %d != %d\n", endi, len(msg.Body)-len(left)-1)
+				}
+				imgMsg.data = left
+
+				ow := root.Get("ow").Int()
+				//oh := root.Get("og").Int()
+				dw := root.Get("dw").Int()
+				dh := root.Get("dh").Int()
+				if ow != dw {
+					clickScale = ow / dw * 1000
+				}
+
+				imgMsg.msg = fmt.Sprintf("Width: %d, Height: %d, Clickscale: %d, Size: %d\n", dw, dh, clickScale, len(msg.Body))
+
+				if !sentSize {
+					dataReader := bytes.NewBuffer([]byte(fmt.Sprintf(`{"type":"frame1","width":%d,"height":%d,"clickScale":%d,"uuid":"%s"}`, dw, dh, clickScale, udid)))
+					http.Post("http://"+coordinator+"/frame", "application/json", dataReader)
+					sentSize = true
+				}
+			} else {
+				imgMsg.data = msg.Body
+			}
+
+			if !discard {
+				imgMsg.imgNum = imgnum
+				imgCh <- imgMsg
+				msg.Free()
+			} else {
+				msg.Free()
+			}
+
+			statLock.Lock()
+			stats.recv++
+			statLock.Unlock()
+
+			imgnum++
+		}
+	}()
+
+	startServer(imgCh, mainCh, &lock, &statLock, &stats, listen_addr, secure, cert, key)
 }
 
-func startWriter(ws *websocket.Conn,imgCh <-chan ImgMsg,writerCh <-chan WriterMsg,lock *sync.RWMutex, statLock *sync.RWMutex, stats *Stats) {
-    go func() {
-        var running bool = true
-        statLock.Lock()
-        stats.socketConnected = true
-        statLock.Unlock()
-        for {
-            select {
-                case imgMsg := <- imgCh:
-                    // Keep receiving images till there are no more to receive
-                    stop := false
-                    for {
-                        select {
-                        case imgMsg = <- imgCh:
-                            
-                        default:
-                            stop = true
-                        }
-                        if stop { break }
-                    }
-            
-                    msg := []byte(imgMsg.msg)
-                    imgBytes := imgMsg.data
-                    
-                    lock.Lock()
-                    ws.WriteMessage(websocket.TextMessage, msg)
-                    ws.WriteMessage(websocket.BinaryMessage, imgBytes )
-                    lock.Unlock()
-                case controlMsg := <- writerCh:
-                    if controlMsg.msg == WriterStop {
-                        running = false
-                    }
-            }
-            if running == false {
-                break
-            }
-        }
-        statLock.Lock()
-        stats.socketConnected = false
-        statLock.Unlock()
-    }()
+func startWriter(ws *websocket.Conn, imgCh <-chan ImgMsg, writerCh <-chan WriterMsg, lock *sync.RWMutex, statLock *sync.RWMutex, stats *Stats) {
+	go func() {
+		var running bool = true
+		statLock.Lock()
+		stats.socketConnected = true
+		statLock.Unlock()
+		for {
+			select {
+			case imgMsg := <-imgCh:
+				// Keep receiving images till there are no more to receive
+				stop := false
+				for {
+					select {
+					case imgMsg = <-imgCh:
+
+					default:
+						stop = true
+					}
+					if stop {
+						break
+					}
+				}
+
+				msg := []byte(imgMsg.msg)
+				imgBytes := imgMsg.data
+
+				lock.Lock()
+				ws.WriteMessage(websocket.TextMessage, msg)
+				ws.WriteMessage(websocket.BinaryMessage, imgBytes)
+				lock.Unlock()
+			case controlMsg := <-writerCh:
+				if controlMsg.msg == WriterStop {
+					running = false
+				}
+			}
+			if running == false {
+				break
+			}
+		}
+		statLock.Lock()
+		stats.socketConnected = false
+		statLock.Unlock()
+	}()
 }
 
-func startServer( imgCh <-chan ImgMsg, mainCh chan<- MainMsg, lock *sync.RWMutex, statLock *sync.RWMutex, stats *Stats, listen_addr string, secure bool, cert string, key string ) (*http.Server) {
-    fmt.Printf("Listening on %s\n", listen_addr )
-    
-    echoClosure := func( w http.ResponseWriter, r *http.Request ) {
-        handleEcho( w, r, imgCh, mainCh, lock, statLock, stats )
-    }
-    statsClosure := func( w http.ResponseWriter, r *http.Request ) {
-        handleStats( w, r, statLock, stats )
-    }
-    rootClosure := func( w http.ResponseWriter, r *http.Request ) {
-        handleRoot( w, r, secure )
-    }
-    
-    server := &http.Server{ Addr: listen_addr }
-    http.HandleFunc( "/echo", echoClosure )
-    http.HandleFunc( "/echo/", echoClosure )
-    http.HandleFunc( "/", rootClosure )
-    http.HandleFunc( "/stats", statsClosure )
-    go func() {
-        if secure {
-            server.ListenAndServeTLS( cert, key );
-        } else {
-            server.ListenAndServe()
-        }
-    }()
-    return server
+func startServer(imgCh <-chan ImgMsg, mainCh chan<- MainMsg, lock *sync.RWMutex, statLock *sync.RWMutex, stats *Stats, listen_addr string, secure bool, cert string, key string) *http.Server {
+	fmt.Printf("Listening on %s\n", listen_addr)
+
+	echoClosure := func(w http.ResponseWriter, r *http.Request) {
+		handleEcho(w, r, imgCh, mainCh, lock, statLock, stats)
+	}
+	statsClosure := func(w http.ResponseWriter, r *http.Request) {
+		handleStats(w, r, statLock, stats)
+	}
+	rootClosure := func(w http.ResponseWriter, r *http.Request) {
+		handleRoot(w, r, secure)
+	}
+
+	server := &http.Server{Addr: listen_addr}
+	http.HandleFunc("/echo", echoClosure)
+	http.HandleFunc("/echo/", echoClosure)
+	http.HandleFunc("/", rootClosure)
+	http.HandleFunc("/stats", statsClosure)
+	go func() {
+		if secure {
+			server.ListenAndServeTLS(cert, key)
+		} else {
+			server.ListenAndServe()
+		}
+	}()
+	return server
 }
 
-func handleStats( w http.ResponseWriter, r *http.Request, statLock *sync.RWMutex, stats *Stats ) {
-    statLock.Lock()
-    recv := stats.recv
-    dumped := stats.dumped
-    socketConnected := stats.socketConnected
-    statLock.Unlock()
-    waitCnt := stats.waitCnt
-    
-    var socketStr string = "no"
-    if socketConnected {
-        socketStr = "yes"
-    }
-    
-    fmt.Fprintf( w, "Received: %d<br>\nDumped: %d<br>\nSocket Connected: %s<br>\nWait Count: %d<br>\n", recv, dumped, socketStr, waitCnt )
+func handleStats(w http.ResponseWriter, r *http.Request, statLock *sync.RWMutex, stats *Stats) {
+	statLock.Lock()
+	recv := stats.recv
+	dumped := stats.dumped
+	socketConnected := stats.socketConnected
+	statLock.Unlock()
+	waitCnt := stats.waitCnt
+
+	var socketStr string = "no"
+	if socketConnected {
+		socketStr = "yes"
+	}
+
+	fmt.Fprintf(w, "Received: %d<br>\nDumped: %d<br>\nSocket Connected: %s<br>\nWait Count: %d<br>\n", recv, dumped, socketStr, waitCnt)
 }
 
-func handleEcho( w http.ResponseWriter, r *http.Request,imgCh <-chan ImgMsg,mainCh chan<- MainMsg, lock *sync.RWMutex, statLock *sync.RWMutex, stats *Stats ) {
-    c, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Print("Upgrade error:", err)
-        return
-    }
-    defer c.Close()
-    fmt.Printf("Received connection\n")
-    welcome(c)
-        
-    writerCh := make(chan WriterMsg, 2)
-    
-    // stop discarding images
-    mainCh <- MainMsg{ msg: EndDiscard }
-    
-    stopped := false
-    
-    c.SetCloseHandler( func( code int, text string ) error {
-        stopped = true
-        writerCh <- WriterMsg{ msg: WriterStop }
-        mainCh   <- MainMsg{ msg: BeginDiscard }
-        return nil
-    } )
-    
-    
-    startWriter(c,imgCh,writerCh,lock,statLock,stats)
-    for {
-        mt, message, err := c.ReadMessage()
-        if err != nil {
-            log.Println("read:", err)
-            break
-        }
-        log.Printf("recv: %s", message)
-        lock.Lock()
-        err = c.WriteMessage(mt, message)
-        lock.Unlock()
-        if err != nil {
-            log.Println("write:", err)
-            break
-        }
-    }
-    
-    // send WriterMsg to terminate writer
-    if !stopped {
-        writerCh <- WriterMsg{ msg: WriterStop }
-        mainCh   <- MainMsg{ msg: BeginDiscard }
-    }
+func handleEcho(w http.ResponseWriter, r *http.Request, imgCh <-chan ImgMsg, mainCh chan<- MainMsg, lock *sync.RWMutex, statLock *sync.RWMutex, stats *Stats) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("Upgrade error:", err)
+		return
+	}
+	defer c.Close()
+	fmt.Printf("Received connection\n")
+	welcome(c)
+
+	writerCh := make(chan WriterMsg, 2)
+
+	// stop discarding images
+	mainCh <- MainMsg{msg: EndDiscard}
+
+	stopped := false
+
+	c.SetCloseHandler(func(code int, text string) error {
+		stopped = true
+		writerCh <- WriterMsg{msg: WriterStop}
+		mainCh <- MainMsg{msg: BeginDiscard}
+		return nil
+	})
+
+	startWriter(c, imgCh, writerCh, lock, statLock, stats)
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		lock.Lock()
+		err = c.WriteMessage(mt, message)
+		lock.Unlock()
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+
+	// send WriterMsg to terminate writer
+	if !stopped {
+		writerCh <- WriterMsg{msg: WriterStop}
+		mainCh <- MainMsg{msg: BeginDiscard}
+	}
 }
 
-func welcome( c *websocket.Conn ) ( error ) {
-    msg := `
+func welcome(c *websocket.Conn) error {
+	msg := `
 {
     "version":1,
     "length":24,
@@ -366,15 +367,15 @@ func welcome( c *websocket.Conn ) ( error ) {
         "tear":false
     }
 }`
-    return c.WriteMessage( websocket.TextMessage, []byte(msg) )
+	return c.WriteMessage(websocket.TextMessage, []byte(msg))
 }
 
-func handleRoot( w http.ResponseWriter, r *http.Request, secure bool ) {
-    if secure {
-        rootTpl.Execute( w, "wss://"+r.Host+"/echo" )
-    } else {
-        rootTpl.Execute( w, "ws://"+r.Host+"/echo" )
-    }
+func handleRoot(w http.ResponseWriter, r *http.Request, secure bool) {
+	if secure {
+		rootTpl.Execute(w, "wss://"+r.Host+"/echo")
+	} else {
+		rootTpl.Execute(w, "ws://"+r.Host+"/echo")
+	}
 }
 
 var rootTpl = template.Must(template.New("").Parse(`
